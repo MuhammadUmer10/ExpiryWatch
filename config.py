fc019ca17ccb84e5f8b24fe9b65d2aaf8374d0f2
MuhammadUmer10 <76046412+MuhammadUmer10@users.noreply.github.com>
2025-07-24 19:23:17 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 SERVER_PORT = 5001
 
-EMAIL_SENDER = 'notify@dictalabs.com'
-EMAIL_PASSWORD = 's%rXxcDZW,^A'
-SMTP_SERVER = 'mail.dictalabs.com'
+EMAIL_SENDER = 'notify@example.com'
+EMAIL_PASSWORD = 'SMTPPASSWORD'
+SMTP_SERVER = 'mail.example.com'
 SMTP_PORT = 465  # 465 for SSL, 587 for TLS
 USE_SSL = True  # True = use SSL, False = use TLS
 
 # Certificate Check Time (24-hour format)
-CERT_CHECK_TIME = "14:53"
-ALERT_SEND_TIME = "14:11"
+CERT_CHECK_TIME = "10:00"
+ALERT_SEND_TIME = "14:00"
 
 # Alert thresholds
-CERT_ALERT_DAYS_BEFORE = 60
+CERT_ALERT_DAYS_BEFORE = 10
 LICENSE_ALERT_DAYS_BEFORE = 10
 
 JWT_SECRET = 'your-jwt-secret-key'
